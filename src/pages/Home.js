c7a9889ad61d824b2ae7d211bd286fe8fcfb4de8
leonzhang <human1108@163.com>
2017-08-31 15:41:38 +0800

    双向绑定

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import 'css/pages/home'
 
 
+export default class extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            obj: {}
+        }
+    }
 
-export default () => (
-    <div>
-        this is home
-    </div>
-)
+    componentDidMount() {
+        const { text, show } = this.refs
+        Object.defineProperty(this.state.obj, 'a', {
+            set: (newVal) => {
+                text.value = newVal
+                show.innerHTML = newVal
+            },
+            get: () => {
+                console.log('fuck')
+            }
+        })
+
+        document.addEventListener('keyup', (e) => {
+            this.state.obj.a = e.target.value
+        })
+
+        setTimeout(() => {
+            console.log(this.state.obj.a)
+        }, 1000)
+    }
+    render() {
+        return (
+            <div>
+                <input ref="text" type='text' />
+                <div ref="show"></div>
+            </div>
+        )
+    }
+
+    
+}
